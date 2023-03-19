1771319872bd1e1301de9157cb67a8be152b17e0
Vinicius Bortolo <luizviniciusbortolo@gmail.com>
2023-03-18 23:01:54 -0300

    feat: create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import logoImg from '../assets/logo.svg';
+
+import styles from './Header.module.css';
+
+export function Header() {
+  return (
+    <header 
+      className={styles.header}
+    >
+      <img 
+        src={logoImg} 
+        alt="Logotipo da aplicação"
+      />
+    </header>
+  );
+}
